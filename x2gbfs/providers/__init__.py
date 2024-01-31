57660ea5582346b0c10353939952d2d439a2a7a9
Holger Bruch <hb@mfdz.de>
2024-01-31 13:45:12 +0100

    add moqo implementation with stadtwerk_tauberfranken

--- x2gbfs/providers/__init__.py
+++ x2gbfs/providers/__init__.py
@@ -3,4 +3,5 @@ from .deer import Deer
 from .example import ExampleProvider
 from .fleetster import FleetsterAPI
 from .lastenvelo_fr import LastenVeloFreiburgProvider
+from .moqo import StadtwerkTauberfrankenProvider
 from .voi_raumobil import VoiRaumobil
--- x2gbfs/x2gbfs.py
+++ x2gbfs/x2gbfs.py
@@ -16,6 +16,7 @@ from x2gbfs.providers import (
     ExampleProvider,
     FleetsterAPI,
     LastenVeloFreiburgProvider,
+    StadtwerkTauberfrankenProvider,
     VoiRaumobil,
 )
 
@@ -44,6 +45,8 @@ def build_extractor(provider: str, feed_config: Dict[str, Any]) -> BaseProvider:
         return VoiRaumobil(api_url, api_user, api_password)
     if provider in ['my-e-car'] or provider.startswith('stadtmobil_'):
         return CantamenIXSIProvider(feed_config)
+    if provider in ['stadtwerk_tauberfranken']:
+        return StadtwerkTauberfrankenProvider(feed_config)
 
     raise ValueError(f'Unknown config {provider}')
 
