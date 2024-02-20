724f0870a36338e9a5f962f647a984148e1bb805
Leonard Ehrenfried <mail@leonard.io>
2024-02-20 21:17:58 +0100

    Take several vehicle types per station into account

--- x2gbfs/providers/noi.py
+++ x2gbfs/providers/noi.py
@@ -6,6 +6,22 @@ import requests, re
 
 logger = logging.getLogger(__name__)
 
+class Counter:
+    """
+    A utiltiy class to group certain cars by their station id.
+    """
+    cars = {}
+    def __init__(self):
+        pass
+
+    def count_car(self, station_id, type):
+        if self.cars.get(station_id) is None:
+            self.cars[station_id] = { type: 1 }
+        elif self.cars[station_id].get(type) is None:
+            self.cars[station_id][type] = 1
+        else:
+            count = self.cars[station_id][type]
+            self.cars[station_id][type] = count + 1
 
 class NoiProvider(BaseProvider):
     """
@@ -24,18 +40,6 @@ class NoiProvider(BaseProvider):
 
     STATION_URL = "https://mobility.api.opendatahub.com/v2/flat%2Cnode/CarsharingStation?limit=500&offset=0&shownull=false&distinct=true"
     CAR_URL = "https://mobility.api.opendatahub.com/v2/flat%2Cnode/CarsharingCar?limit=500&offset=0&shownull=false&distinct=true"
-
-    VEHICLE_TYPES = {
-        'scooter': {
-            # See https://github.com/MobilityData/gbfs/blob/v2.3/gbfs.md#vehicle_typesjson
-            'vehicle_type_id': 'vw-golf',
-            'form_factor': 'car',
-            'propulsion_type': 'combustion',
-            'max_range_meters': 500000,
-            'name': 'VW Golf',
-            'wheel_count': 4
-        }
-    }
 
     def __init__(self):
         pass
@@ -70,18 +74,6 @@ class NoiProvider(BaseProvider):
 
 
     def load_stations(self, default_last_reported: int) -> Tuple[Optional[Dict], Optional[Dict]]:
-        """
-        Retrieves stations from the providers API and converts them
-        into gbfs station infos and station status.
-        Returns dicts where the key is the station_id and values
-        are station_info/station_status.
-
-        For free floating only providers, this method needs not to be overwritten.
-
-        Note: station status' vehicle availabilty currently will be calculated
-        using vehicle information's station_id, in case it is defined by this
-        provider.
-        """
 
         response = requests.get(self.STATION_URL, timeout=20)
         raw_stations = response.json()["data"]
@@ -104,22 +96,33 @@ class NoiProvider(BaseProvider):
         response = requests.get(self.CAR_URL, timeout=20)
         raw_cars = response.json()["data"]
         infos = {}
+        counter = Counter()
         for i in raw_cars:
 
             if i.get("pcode") is not None:
                 id = i["pcode"]
-                infos[id] = {
-                    "station_id": id,
-                    "num_bikes_available" : 1,
-                    "is_installed": True,
-                    "is_renting": True,
-                    "is_returning": True,
-                    "last_reported": last_reported,
-                    "vehicle_types_available": [{
-                        "vehicle_type_id": self.extract_type_id(i),
-                        "count": 1
-                    }]
-                }
+                type_id = self.extract_type_id(i)
+                counter.count_car(id, type_id)
+
+        for station_id, counts in counter.cars.items():
+
+            avail = []
+
+            for type_id in counts:
+                avail.append({
+                    "vehicle_type_id": type_id,
+                    "count": counts[type_id]
+                })
+
+            infos[station_id] = {
+                "station_id": station_id,
+                "num_bikes_available" : 1,
+                "is_installed": True,
+                "is_renting": True,
+                "is_returning": True,
+                "last_reported": last_reported,
+                "vehicle_types_available": avail
+        }
         return infos
 
 
