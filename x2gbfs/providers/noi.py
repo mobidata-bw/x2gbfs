b654a08327defe41db0c1d05ac0eb32233f4e1d0
Leonard Ehrenfried <mail@leonard.io>
2024-02-27 16:14:33 +0100

    Fix lint errors

--- x2gbfs/providers/noi.py
+++ x2gbfs/providers/noi.py
@@ -1,8 +1,8 @@
 import logging
+import requests
+import re
 from typing import Dict, Optional, Tuple
-
 from x2gbfs.gbfs.base_provider import BaseProvider
-import requests, re
 
 logger = logging.getLogger(__name__)
 
@@ -27,22 +27,22 @@ class Counter:
         cars = self.cars.get(station_id)
         if cars is None:
             return 0
-        else:
-            count = 0
-            for i in cars:
-                count = count + cars[i]
+
+        count = 0
+        for i in cars:
+            count = count + cars[i]
         return count
 
 class NoiProvider(BaseProvider):
-    STATION_URL = "https://mobility.api.opendatahub.com/v2/flat%2Cnode/CarsharingStation?limit=500&offset=0&shownull=false&distinct=true"
-    CAR_URL = "https://mobility.api.opendatahub.com/v2/flat%2Cnode/CarsharingCar?limit=500&offset=0&shownull=false&distinct=true"
+    STATION_URL = 'https://mobility.api.opendatahub.com/v2/flat%2Cnode/CarsharingStation?limit=500&offset=0&shownull=false&distinct=true'
+    CAR_URL = 'https://mobility.api.opendatahub.com/v2/flat%2Cnode/CarsharingCar?limit=500&offset=0&shownull=false&distinct=true'
 
     def __init__(self):
         pass
 
     def load_vehicles(self, default_last_reported: int) -> Tuple[Optional[Dict], Optional[Dict]]:
         response = requests.get(self.CAR_URL, timeout=20)
-        raw_cars = response.json()["data"]
+        raw_cars = response.json()['data']
         types = {}
         for i in raw_cars:
 
@@ -53,38 +53,36 @@ class NoiProvider(BaseProvider):
                     'form_factor': 'car',
                     'propulsion_type': self.extract_propulsion(i),
                     'max_range_meters': 500000,
-                    'name': i["smetadata"]["brand"].strip(),
+                    'name': i['smetadata']['brand'].strip(),
                     'wheel_count': 4
                 }
         return types, {}
 
     def extract_type_id(self, i):
-        output = i["smetadata"]["brand"].lower().strip().replace("!", "")
-        output =  re.sub(r'[^a-z0-9]+', '-', output)
-        output =  re.sub(r'-$', '', output)
-        return output
+        output = i['smetadata']['brand'].lower().strip().replace('!', '')
+        output = re.sub(r'[^a-z0-9]+', '-', output)
+        return re.sub(r'-$', '', output)
 
     def extract_propulsion(self, i):
-        if(self.extract_type_id(i).__contains__("elektro")):
-            return "electric"
-        else:
-            return "combustion"
+        if(self.extract_type_id(i).__contains__('elektro')):
+            return 'electric'
+        return 'combustion'
 
 
     def load_stations(self, default_last_reported: int) -> Tuple[Optional[Dict], Optional[Dict]]:
 
         response = requests.get(self.STATION_URL, timeout=20)
-        raw_stations = response.json()["data"]
+        raw_stations = response.json()['data']
 
         info = {}
         for i in raw_stations:
-            id = i["scode"]
-            coord = i["scoordinate"]
+            id = i['scode']
+            coord = i['scoordinate']
             info[id] = {
-                "station_id": id,
-                "name": i["sname"],
-                "lon": coord["x"],
-                "lat": coord["y"],
+                'station_id': id,
+                'name': i['sname'],
+                'lon': coord['x'],
+                'lat': coord['y'],
             }
 
         status = self.load_status(default_last_reported)
@@ -92,13 +90,13 @@ class NoiProvider(BaseProvider):
 
     def load_status(self, last_reported: int) -> Optional[Dict]:
         response = requests.get(self.CAR_URL, timeout=20)
-        raw_cars = response.json()["data"]
+        raw_cars = response.json()['data']
         statuses = {}
         counter = Counter()
         for i in raw_cars:
 
-            if i.get("pcode") is not None:
-                id = i["pcode"]
+            if i.get('pcode') is not None:
+                id = i['pcode']
                 type_id = self.extract_type_id(i)
                 counter.count_car(id, type_id)
 
@@ -108,17 +106,17 @@ class NoiProvider(BaseProvider):
 
             for type_id in counts:
                 avail.append({
-                    "vehicle_type_id": type_id,
-                    "count": counts[type_id]
+                    'vehicle_type_id': type_id,
+                    'count': counts[type_id]
                 })
 
             statuses[station_id] = {
-                "station_id": station_id,
-                "num_bikes_available" : counter.total_cars(station_id),
-                "is_installed": True,
-                "is_renting": True,
-                "is_returning": True,
-                "last_reported": last_reported,
-                "vehicle_types_available": avail
+                'station_id': station_id,
+                'num_bikes_available' : counter.total_cars(station_id),
+                'is_installed': True,
+                'is_renting': True,
+                'is_returning': True,
+                'last_reported': last_reported,
+                'vehicle_types_available': avail
         }
         return statuses
