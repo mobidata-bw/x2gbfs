8af6f7b1f80aa85ff5c5399f162e79e92f474818
Thorsten Fröhlinghaus <thorsten.froehlinghaus@nvbw.de>
2024-10-18 11:43:56 +0200

    zeag energie carsharig

--- x2gbfs/providers/__init__.py
+++ x2gbfs/providers/__init__.py
@@ -4,7 +4,6 @@ from .example import ExampleProvider
 from .fleetster import FleetsterAPI
 from .flinkster import FlinksterProvider
 from .lastenvelo_fr import LastenVeloFreiburgProvider
-from .moqo import StadtwerkTauberfrankenProvider
-from .moqo import ZeagEnergieProvider
+from .moqo import StadtwerkTauberfrankenProvider, ZeagEnergieProvider
 from .noi import NoiProvider
 from .voi_raumobil import VoiRaumobil
