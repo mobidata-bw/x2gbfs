16fcc348961138e72dd3da9783a63d9d7fb09c26
Thorsten Fröhlinghaus <thorsten.froehlinghaus@nvbw.de>
2025-05-22 16:34:15 +0200

    add lara_to_go provider

--- x2gbfs/x2gbfs.py
+++ x2gbfs/x2gbfs.py
@@ -55,7 +55,7 @@ def build_extractor(provider: str, feed_config: Dict[str, Any]) -> BaseProvider:
     if provider.startswith('cambio_'):
         return CambioProvider(feed_config)
     if (
-        provider in ['naturenergie_sharing', 'oekostadt_renningen', 'gruene-flotte_freiburg', 'lara_to_go']
+        provider in ['naturenergie_sharing', 'oekostadt_renningen', 'gruene-flotte_freiburg']
         or provider.startswith('stadtmobil_')
         or provider.startswith('teilauto_')
     ):
@@ -66,6 +66,7 @@ def build_extractor(provider: str, feed_config: Dict[str, Any]) -> BaseProvider:
         'oberschwabenmobil',
         'gmuend_bewegt',
         'coono',
+        'lara_to_go',
     ]:
         return MoqoProvider(feed_config)
     if provider in ['noi']:
