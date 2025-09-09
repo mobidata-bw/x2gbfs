2aebb0652a2fd0135d9d65c2ffa67b91398d60e1
Thorsten Fröhlinghaus <thorsten.froehlinghaus@nvbw.de>
2025-09-09 15:49:19 +0200

    set cargo biycle max range to 50km

--- x2gbfs/providers/moqo_einfach_unterwegs.py
+++ x2gbfs/providers/moqo_einfach_unterwegs.py
@@ -9,7 +9,7 @@ class EinfachUnterwegsProvider(MoqoProvider):
         id = super()._extract_vehicle_type(vehicle_types, vehicle)
         if vehicle_types[id]['name'] == 'Urban Arrow Family':
             vehicle_types[id]['form_factor'] = 'cargo_bicycle'
-            vehicle_types[id]['max_range_meters'] = 100000
+            vehicle_types[id]['max_range_meters'] = 50000
             vehicle_types[id]['make'] = 'Urban Arrow'
             vehicle_types[id]['model'] = 'Family'
             vehicle_types[id]['cargo_load_capacity'] = 125
